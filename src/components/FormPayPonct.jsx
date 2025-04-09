56efad8efe1130923c432ffc9ea203516f8f4232
jujulith11 <juniaskokon17@gmail.com>
2025-04-09 06:35:28 +0000

    Connexion du formulaire au back

--- src/components/FormPayPonct.jsx
+++ src/components/FormPayPonct.jsx
@@ -17,12 +17,50 @@ const FormPayPonct = () => {
         }));
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("✅ Données envoyées :", {
-            ...formData,
-        });
+    
+        const payload = {
+            nom: formData.nom,
+            prenom: formData.prenoms,
+            email: formData.email,
+            telephone: formData.whatsapp,
+            montant: formData.montant,
+        };
+        const API_URL = process.env.REACT_APP_API_URL;
+        try {
+            const response = await fetch(`{$API_URL}/api/soutiens/create-with-participant`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload),
+            });
+    
+            const data = await response.json();
+            if (response.ok && data.redirectUrl) {
+                // Redirection vers la page de paiement CinetPay
+                window.open(data.redirectUrl,'_blank');
+            
+
+                console.log('Participant et soutien créés:', data);
+                // : reset du form
+                setFormData({
+                    nom: '',
+                    prenoms: '',
+                    email: '',
+                    whatsapp: '',
+                    montant: '',
+                });
+            } else {
+                console.error('Erreur lors de la création:', data.error);
+            }
+    
+        } catch (err) {
+            console.error('Erreur réseau:', err);
+        }
     };
+    
 
     return (
         <form
@@ -38,6 +76,7 @@ const FormPayPonct = () => {
                     className="outline-none border p-3 rounded w-full mb-8 focus:ring-2 focus:border-0 focus:ring-normal-purple/60"
                     value={formData.nom}
                     onChange={handleChange}
+                    required
                 />
                 <input
                     type="text"
@@ -46,6 +85,7 @@ const FormPayPonct = () => {
                     className="outline-none border p-3 rounded w-full mb-8 focus:ring-2 focus:border-0 focus:ring-normal-purple/60"
                     value={formData.prenoms}
                     onChange={handleChange}
+                    required
                 />
                 <input
                     type="email"
@@ -54,6 +94,7 @@ const FormPayPonct = () => {
                     className="outline-none border p-3 rounded w-full mb-8 focus:ring-2 focus:border-0 focus:ring-normal-purple/60"
                     value={formData.email}
                     onChange={handleChange}
+                    required
                 />
                 <input
                     type="text"
@@ -62,6 +103,7 @@ const FormPayPonct = () => {
                     className="outline-none border p-3 rounded w-full mb-8 focus:ring-2 focus:border-0 focus:ring-normal-purple/60"
                     value={formData.whatsapp}
                     onChange={handleChange}
+                    required
                 />
             </div>
 
@@ -73,6 +115,7 @@ const FormPayPonct = () => {
                 className="outline-none border p-3 rounded w-full mb-8 focus:ring-2 focus:border-0 focus:ring-normal-purple/60"
                 value={formData.montant}
                 onChange={handleChange}
+                required
             />
 
             {/* Valider le paiement */}
