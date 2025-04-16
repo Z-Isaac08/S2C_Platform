3925b0a7ba25059a9185d368ab7161e524a1e949
Z-Isaac08 <ezechielncho225@gmail.com>
2025-04-16 14:09:46 +0000

    fix_v04

--- Server/Utils/sendWhatsapp.js
+++ Server/Utils/sendWhatsapp.js
@@ -1,27 +1,27 @@
-require('dotenv').config();
-const twilio = require('twilio');
-// Tes identifiants Twilio
-const accountSid = process.env.ACCOUNT_SID;
-const authToken = process.env.AUTH_TOKEN_TWILIO; // Remplace par ton vrai token
-const client = twilio(accountSid, authToken);
+// require('dotenv').config();
+// const twilio = require('twilio');
+// // Tes identifiants Twilio
+// const accountSid = process.env.ACCOUNT_SID;
+// const authToken = process.env.AUTH_TOKEN_TWILIO; // Remplace par ton vrai token
+// const client = twilio(accountSid, authToken);
 
-// Envoie le QR code au participant via WhatsApp.
-const envoyerQRparWhatsApp = async (telephone, qrCodeUrl) => {
-    try {
-        const message = await client.messages.create({
-            from: 'whatsapp:+14155238886',
-            to: `whatsapp:${telephone}`,
-            body: `Bienvenue au S2C #3 ! Voici ton QR Code. Merci pour ton inscription`,
-            mediaUrl: [qrCodeUrl]
-        });
+// // Envoie le QR code au participant via WhatsApp.
+// const envoyerQRparWhatsApp = async (telephone, qrCodeUrl) => {
+//     try {
+//         const message = await client.messages.create({
+//             from: 'whatsapp:+14155238886',
+//             to: `whatsapp:${telephone}`,
+//             body: `Bienvenue au S2C #3 ! Voici ton QR Code. Merci pour ton inscription`,
+//             mediaUrl: [qrCodeUrl]
+//         });
 
-        console.log('Message envoyé ! SID :', message.sid);
-        console.log({telephone, qrCodeUrl});
-        return message.sid;
-    } catch (err) {
-        console.error('Erreur lors de l’envoi du message WhatsApp :', err.message);
-        throw err;
-    }
-};
+//         console.log('Message envoyé ! SID :', message.sid);
+//         console.log({telephone, qrCodeUrl});
+//         return message.sid;
+//     } catch (err) {
+//         console.error('Erreur lors de l’envoi du message WhatsApp :', err.message);
+//         throw err;
+//     }
+// };
 
-module.exports = envoyerQRparWhatsApp;
+// module.exports = envoyerQRparWhatsApp;
